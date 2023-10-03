cfee4a6c40e3d3fe8229415c536c999860ad55bc
Wadagraprana <115563671+Wadagraprana@users.noreply.github.com>
2023-10-03 22:37:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ form.addEventListener('submit', function (e) {
         });
     }
 });
+
+
+function hanyaAngka(evt) {
+    var charcode = (evt.which) ? evt.which : event.keycode
+    if (charcode > 31 && (charcode < 48 || charcode >57))
+        return false;
+    return true;
+}
